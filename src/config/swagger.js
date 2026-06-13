af9434ad9c64d8fefb24479270a6afbb251cf1ec
Ankit yadav <ankityadav15202@gmail.com>
2026-06-13 14:21:07 +0530

    chore: initialize project and configuration files

---
+++ src/config/swagger.js
@@ -0,0 +1,26 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Task Management API',
+      version: '1.0.0',
+      description: 'REST API for Task Management Platform',
+    },
+    servers: [{ url: '/api' }],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+module.exports = swaggerJsdoc(options);
